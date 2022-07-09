0afa95827cd5d7ce4f6d00941264e0d5545b0529
Kevin Baumgartner <kevjbaumgartner@gmail.com>
2022-07-09 18:53:10 -0400

    Updated for NPM publishing
    Modified the file structure and setup package.json for publishing.

---
+++ react-animated-text-wipe/dist/components/animated-text-wipe.js
@@ -0,0 +1,53 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+require("core-js/modules/es.object.assign.js");
+
+var _react = _interopRequireDefault(require("react"));
+
+require("./styles/AnimatedTextWipe.css");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+const AnimatedTextWipe = props => {
+  const firstColor = props.firstColor;
+  const secondColor = props.secondColor;
+  const textColor = props.textColor;
+  const difference = props.difference;
+  const antialiasing = props.antialiasing;
+  const direction = props.direction;
+  const speed = props.speed;
+  const styles = {
+    AnimatedTextWipeOuter: {
+      'background': 'linear-gradient(to right, #' + firstColor + ' 0%, #' + firstColor + ' 33%, #' + secondColor + ' 33%, #' + secondColor + ' 66%, #' + firstColor + ' 66%, #' + firstColor + ' 100%)',
+      'backgroundSize': '303% 100%',
+      'width': 'fit-content'
+    },
+    AnimatedTextWipeInner: {
+      'color': '#' + textColor
+    },
+    MBM: {
+      'mixBlendMode': difference === 'true' ? 'difference' : 'unset'
+    },
+    AA: {
+      'backgroundColor': '#' + textColor,
+      'WebkitBackgroundClip': 'text',
+      'WebkitTextFillColor': 'transparent'
+    },
+    Animation: {
+      'animation': direction + ' ' + speed + 's linear 0s infinite normal'
+    }
+  };
+  return /*#__PURE__*/_react.default.createElement("div", {
+    style: Object.assign({}, styles.AnimatedTextWipeOuter, styles.Animation)
+  }, /*#__PURE__*/_react.default.createElement("div", {
+    style: Object.assign({}, styles.AnimatedTextWipeInner, difference === 'true' ? styles.MBM : '', antialiasing === 'true' ? styles.AA : '')
+  }, props.children));
+};
+
+var _default = AnimatedTextWipe;
+exports.default = _default;
---
+++ react-animated-text-wipe/dist/index.js
@@ -0,0 +1,15 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "AnimatedTextWipe", {
+  enumerable: true,
+  get: function get() {
+    return _animatedTextWipe.default;
+  }
+});
+
+var _animatedTextWipe = _interopRequireDefault(require("./components/animated-text-wipe"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
